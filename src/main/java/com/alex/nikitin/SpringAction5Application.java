99576b6a0b64962e7b4aa19ebf92f44fcbc1a167
nikitinalexx <nikitinalexx22@gmail.com>
2019-05-27 22:56:23 +0300

    Initial commit

---
+++ src/main/java/com/alex/nikitin/SpringAction5Application.java
@@ -0,0 +1,13 @@
+package com.alex.nikitin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAction5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAction5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/alex/nikitin/SpringAction5ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alex.nikitin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAction5ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
